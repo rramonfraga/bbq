b73eb27eaec729e7a141790446f2e9d66cacafb0
Rafael Ramon Fraga <rafael_ramon_fraga@yahoo.es>
2015-12-04 11:32:27 +0100

    First Iteration Complete

---
+++ app/controllers/barbecues_controller.rb
@@ -0,0 +1,29 @@
+class BarbecuesController < ApplicationController
+  def index
+    if current_user
+      @barbecues = Barbecue.order(:date)
+    else
+      redirect_to 'users/sign_in' 
+    end
+  end
+
+  def show
+    # Don't add any code to this show action.
+    # Implement your barbecue API in another action.
+  end
+
+  def new
+    @bbq = Barbecue.new
+  end
+
+  def create
+    bbq_params = params.require(:barbecue).permit(:title, :venue, :date)
+    @bbq = Barbecue.new(bbq_params)
+
+    unless @bbq.save
+      render(:new)
+    else
+      redirect_to(barbecues_path)
+    end
+  end
+end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,8 @@
+class UserController < ApplicationController
+  before_action :authenticate_user!, only: :show
+
+  def show
+    @user = User.find params[:id]
+    render 'users/show'
+  end
+end
